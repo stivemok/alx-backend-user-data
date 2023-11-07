5956a5d1b1b09bd7ed63ec211e60f72b605a44b6
stivemok <stivemok@gmail.com>
2023-11-07 15:20:25 +0300

    Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,50 +1,23 @@
 #!/usr/bin/env python3
-"""Route module for the API"""
+"""
+Route module for the API
+"""
 from os import getenv
 from api.v1.views import app_views
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
+from flask_cors import CORS, cross_origin
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = os.getenv('AUTH_TYPE')
+auth = None
 
-auth_repo = {
-    'auth': Auth,
-    'basic_auth': BasicAuth
-}
-
-if auth:
-    try:
-        auth = auth_repo[auth]()
-    except Exception:
-        auth = None
-
-
-@app.before_request
-def filter():
-    """checks if auth is enabled"""
-    if auth is None:
-        return
-    path = request.path
-    excluded_paths = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
-    ]
-    if auth.require_auth(path, excluded_paths) is False:
-        return
-
-    if auth.authorization_header(request) is None:
-        abort(401)
-
-    if auth.current_user(request) is None:
-        abort(403)
+if getenv("AUTH_TYPE") == "auth":
+    auth = Auth()
+# elif getenv("AUTH_TYPE") == "basic_auth":
+#     auth = BasicAuth()
 
 
 @app.errorhandler(404)
@@ -54,18 +27,31 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def not_authorized(error) -> str:
-    """Not authorized handler"""
+def unauthorized(error) -> str:
+    """401 unauthorized handler"""
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """Forbidden action handler"""
+    """403 unauthorized handler"""
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """before request handler"""
+    authorized = ['/api/v1/status/', '/api/v1/unauthorized/',
+                  '/api/v1/forbidden/']
+
+    if auth and auth.require_auth(request.path, authorized):
+        if not auth.authorization_header(request):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
-"""Module of Index views"""
+""" Module of Index views
+"""
 from flask import jsonify, abort
 from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """GET /api/v1/status
-    Return: the status of the API"""
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
+    """
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """GET /api/v1/stats
-    Return: the number of each objects"""
+    """ GET /api/v1/stats
+    Return:
+      - the number of each objects
+    """
     from models.user import User
     stats = {}
     stats['users'] = User.count()
@@ -22,14 +27,14 @@ def stats() -> str:
 
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def abort_401() -> str:
-    """GET /api/v1/unauthorized
-    Return: test unauthorized endpoint"""
+def unauthorized() -> str:
+    """ GET /api/V1/unauthorized
+        return aborts"""
     abort(401)
 
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def abort_403() -> str:
-    """GET /api/v1/forbidden
-    Return: test forbidden endpoint"""
+def forbidden() -> str:
+    """GET /api/V1/forbidden
+        return aborts"""
     abort(403)
